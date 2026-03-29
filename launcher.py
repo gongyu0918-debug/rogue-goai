7857668bb77238e4e08f5733fc9d921881c4b031
gongyu0918-debug <gongyu0918-debug@users.noreply.github.com>
2026-03-30 02:43:54 +0800

    Bump packaged server revision for v0.1.4

--- launcher.py
+++ launcher.py
@@ -24,7 +24,7 @@ from tkinter import scrolledtext, messagebox
 from urllib.parse import urlencode
 
 SERVER_URL = "http://localhost:8000"
-EXPECTED_SERVER_REV = "20260329-legacy-compat"
+EXPECTED_SERVER_REV = "20260330-bugfix-package"
 KATAGO_REPO = "lightvector/KataGo"
 
 
